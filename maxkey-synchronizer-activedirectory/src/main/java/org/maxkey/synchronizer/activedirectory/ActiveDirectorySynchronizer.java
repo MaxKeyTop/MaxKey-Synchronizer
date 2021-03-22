484166c75b0f6c33ab96410898981e3ad111cc41
MaxKey <shimingxy@qq.com>
2021-03-22 18:13:38 +0800

    Update ActiveDirectorySynchronizer.java

--- maxkey-synchronizer-activedirectory/src/main/java/org/maxkey/synchronizer/activedirectory/ActiveDirectorySynchronizer.java
+++ maxkey-synchronizer-activedirectory/src/main/java/org/maxkey/synchronizer/activedirectory/ActiveDirectorySynchronizer.java
@@ -1,8 +1,10 @@
 package org.maxkey.synchronizer.activedirectory;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+@SpringBootApplication
 public class ActiveDirectorySynchronizer {
 	
 	public static void main(String[] args) {
